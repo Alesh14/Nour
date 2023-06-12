c68ea7d314ee6105f3ce483ea0f436ea94e290ae
Alisher Manatbek <alisher.manatbek2004@gmail.com>
2023-06-12 11:38:08 +0600

    commit
    added: Controllers group, MainScreen (UITabBarController)
    updated: SceneDelegate

---
+++ Nour/Controllers/MainScreen.swift
@@ -0,0 +1,29 @@
+//
+//  MainScreen.swift
+//  Nour
+//
+//  Created by Alisher Manatbek on 12.06.2023.
+//
+
+import UIKit
+
+class MainScreen: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
